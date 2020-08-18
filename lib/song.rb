93dcf80d242b8387f8e348115744f920ee7cbba8
Lauren Anderson <laurenaanderson95@gmail.com>
2020-08-18 22:14:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -50,12 +50,6 @@ class Song
     end
   end
   
-  def self.new_from_filename(filename)
-    data = filename.split(” - “)
-    artist = Artist.find_or_create_by_name(data[0])
-    genre = Genre.find_or_create_by_name(data[2].chomp(“.mp3”))
-    song = self.new(data[1], artist, genre)
-  end
  
   
 end
