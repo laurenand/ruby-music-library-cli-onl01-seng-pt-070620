ebeae414d61f380ec12ecd6157833343f9a1114a
Lauren Anderson <laurenaanderson95@gmail.com>
2020-08-18 22:46:26 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,5 +1,13 @@
 module Concerns
   module Findable
-  
+    
+    def find_by_name(name)
+      self.all.find {|instance| instance.name == name}
+    end
+    
+    def find_or_create_by_name
+      
+    end
+    
   end
 end
