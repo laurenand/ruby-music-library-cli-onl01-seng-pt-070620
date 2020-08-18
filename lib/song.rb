33d8a69b4aa139db06b284848f3fbd978d71685f
Lauren Anderson <laurenaanderson95@gmail.com>
2020-08-18 21:55:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,6 +40,15 @@ class Song
   def self.find_by_name(name)
     self.all.detect {|song| song.name == name}
   end
+  
+  def self.find_or_create_by_name(name)
+    artist = Artist.all.find {|artist| artist.name == name}
+    if !artist.nil?
+      artist
+    else
+      Artist.new(name)
+    end
+  end
  
   
 end
