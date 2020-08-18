7306549f9e8b1e7c4db33dedf34aac5874d3ea59
Lauren Anderson <laurenaanderson95@gmail.com>
2020-08-18 22:08:59 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,7 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
-    self.find_by_name(name) || self.create(name)
+    self.find_by_name(name) || self.create(song)
     binding.pry
   end
   
