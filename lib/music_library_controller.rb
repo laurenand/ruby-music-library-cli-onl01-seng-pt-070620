1ad19a550ca5f8f6502f51e1671a9989921568ff
Lauren Anderson <laurenaanderson95@gmail.com>
2020-08-18 23:41:16 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -41,7 +41,7 @@ class MusicLibraryController
   
   def list_artists
     sorted_artists = Artist.all.sort {|a, b| a.name <=> b.name}
-    sorted_artists.each_with_index {|song, i| puts "#{i + 1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"}
+    sorted_artists.each_with_index {|artist, i| puts "#{i + 1}. #{artist.artist.name} - #{song.name} - #{song.genre.name}"}
   end
 
 end
