121ada80f4cdcab72663ab42f322854352b9d26f
Lauren Anderson <laurenaanderson95@gmail.com>
2020-08-19 00:15:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -28,8 +28,8 @@ class Artist
   end
   
   def add_song(song)
-    song.artist = self if song.artist == nil
-    self.songs << song unless @songs.include?(song)
+    song.artist = self unless song.artist
+    self.songs << song unless songs.include?(song)
   end
   
   def genres 
